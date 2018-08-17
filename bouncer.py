b3c771a331229c1dd6fa5fae48f1b11f68dbb4f2
Adam Kahn <adamkahn@adamkahn-macbookpro.roam.corp.google.com>
2018-08-17 15:14:34 -0700

    Refactor to simpler logical statements

--- bouncer.py
+++ bouncer.py
@@ -5,14 +5,12 @@ age = input("How old are you? ")
 if age:
 	#convert age input to integer
 	age = int(age)
-	# 18-20 gets marked as underage
-	if age >= 18 and age <= 20:
-		print("You can enter but you'll be marked as underage")
-	# 21+ gets wristband to drink
-	elif age >= 21:
+	if age >= 21:
 		print("You can enter and can drink!")
-	# < 18 is too young
+	elif age >= 18:
+		print("You can enter but you'll be marked as underage")
 	else:
 		print("You cannot come in")
+#prompt to enter an age
 else:
 	print("Please enter an age!")
