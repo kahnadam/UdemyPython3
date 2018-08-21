7c8f05ac9ab5eb1ff26579faccf0d4d95cff0654
Adam Kahn <adamkahn@adamkahn-macbookpro.roam.corp.google.com>
2018-08-21 15:56:15 -0700

    Add f-string to reveal what computer played

--- rps2.py
+++ rps2.py
@@ -14,6 +14,7 @@ print("...rock...")
 print("...paper...")
 print("...scissors...")
 player = input("Enter your choice: ")
+print(f"Computer plays {computer}")
 
 #find winner
 if player == computer:
