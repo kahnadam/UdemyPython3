8ee4caffb391a5e5ed46c4f64a9e6f9406f20fb0
Adam Kahn <adamkahn@adamkahn-macbookpro.roam.corp.google.com>
2018-08-24 16:35:36 -0700

    Add second way of writing code in a comment

--- rand_num.py
+++ rand_num.py
@@ -15,3 +15,26 @@ while play == "y":
 	play = input("You got it! Do you want to play again? (y/n) ")
 
 print("Okay! Goodbye!")
+
+#another way to do it, with a break:
+"""
+from random import randint
+
+random_number = randint(1,10)
+
+while True:
+	guess = int(input("Choose a number between 1 and 10: "))
+	if guess < random_number:
+		print("Too low! Guess again")
+	elif guess > random_number:
+		print("Too high! Guess again")
+	else:
+		print("You got it!")
+		play_again = input("Do you want to play again? (y/n) ")
+		if play_again == "y":
+			random_number = randint(1,10)
+			guess = None
+		else:
+			print("Okay! Goodbye!")
+			break
+"""
