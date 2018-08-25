891b7099957a535b07d7c8526ec996cc1e06e9af
Adam Kahn <adamkahn@adamkahn-macbookpro.roam.corp.google.com>
2018-08-24 17:52:49 -0700

    Try reverting commit

--- intro_to_conditionals/conditional_example.py
+++
@@ -1,9 +0,0 @@
-print("Enter a name")
-name = input()
-if name == 'Arya Stark':
-	print('Valar Morghulis')
-elif name == 'Jon Snow':
-	print('You know nothing')
-else:
-	print('Carry on')
-
--- intro_to_conditionals/truthiness.py
+++
@@ -1,9 +0,0 @@
-animal = input("Enter your favorite animal: ")
-
-#an empty string is inherently false
-#that's why we can say --> if animal:
-#instead of something else to check if the string is empty
-if animal:
-	print(animal + " is my favorite too!")
-else:
-	print("You didn't say anything")
