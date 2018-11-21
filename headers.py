2b85409329881614424663ae13d154b0127847b9
Adam Kahn <adamkahn@adamkahn-macbookpro.roam.corp.google.com>
2018-11-21 12:27:43 -0800

    Add in conversion from JSON to Python

--- headers.py
+++ headers.py
@@ -3,6 +3,10 @@ import requests
 url = "https://icanhazdadjoke.com"
 
 #get the plain text version of the site
-response = requests.get(url, headers={"Accept": "text/plain"})
+response = requests.get(url, headers={"Accept": "application/JSON"})
 
+#returns the data as a string
 print(response.text)
+
+#take the JSON response and convert into Python
+print(response.json())
