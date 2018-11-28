d8a24ddaaef4467125d2fad81cb03bfbff7f08d7
Adam Kahn <adamkahn@adamkahn-macbookpro.roam.corp.google.com>
2018-11-28 12:51:33 -0800

    Add params to search

--- params.py
+++ params.py
@@ -3,13 +3,20 @@ import requests
 url = "https://icanhazdadjoke.com/search"
 
 #get the plain text version of the site
-response = requests.get(url, headers={"Accept": "application/JSON"})
+response = requests.get(
+    url,
+    headers={"Accept": "application/JSON"},
+    params={
+        "term": "cat"
+    }
+)
 
 #set a variable that's a Python dictionary of the JSON data
 data = response.json()
+print(data)
 
 #print the joke from the Python dictionary
-print(data["joke"])
+#print(data["joke"])
 
 #print response status code
-print(f"status: {data['status']}")
+#print(f"status: {data['status']}")
