05cc0a965b8b6e4c2e69a5e1e9781b6a567de618
Adam Kahn <adamkahn@adamkahn-macbookpro.roam.corp.google.com>
2018-11-29 18:48:17 -0800

    Create method to return user initials

--- init.py
+++ init.py
@@ -8,7 +8,11 @@ class User:
     def full_name(self):
         return f"{self.first} {self.last}"
 
+    def initials(self):
+        return f"{self.first[0]}.{self.last[0]}."
+
 user1 = User("Joe","Smith",68)
 user2 = User("Biana","Lopez",31)
 
 print(user2.full_name())
+print(user1.initials())
