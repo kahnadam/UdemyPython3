91f1ae54c498ec12b070386bb3cac6cacfa8cc99
Adam Kahn <adamkahn@adamkahn-macbookpro.roam.corp.google.com>
2018-11-29 18:57:27 -0800

    Add age method

--- init.py
+++ init.py
@@ -16,9 +16,29 @@ class User:
     def likes(self,thing):
         return f"{self.first} likes {thing}"
 
+    #boolean method to check if object is a senior citizen
+    def is_senior(self):
+        return self.age >= 65
+
+    def birthday(self):
+        self.age += 1
+        return f"Happy {self.age} birthday, {self.first}"
+
+#instantiate two objects
 user1 = User("Joe","Smith",68)
-user2 = User("Biana","Lopez",31)
+user2 = User("Bianca","Lopez",31)
 
 print(user2.full_name())
 print(user1.initials())
+
+#for the likes method you need to pass in a thing
 print(user2.likes("ice cream"))
+
+#is_senior is a boolean method to see if user is a is_senior
+print(user1.is_senior())
+print(user2.is_senior())
+
+#age
+print(user1.age)
+print(user1.birthday())
+print(user1.age)
