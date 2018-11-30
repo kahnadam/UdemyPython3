d9e649c2427a4879287452cbd1b8b2e6a1a64a3a
Adam Kahn <adamkahn@adamkahn-macbookpro.roam.corp.google.com>
2018-11-29 18:51:37 -0800

    Create instance method to return string of what user likes

--- init.py
+++ init.py
@@ -4,15 +4,21 @@ class User:
         self.last = last
         self.age = age
 
-    #make a method to return first and last name
+    #make an instance method to return first and last name
     def full_name(self):
         return f"{self.first} {self.last}"
 
+    #make an instance method to return initials
     def initials(self):
         return f"{self.first[0]}.{self.last[0]}."
+
+    #make an instance method to return user liking a thing
+    def likes(self,thing):
+        return f"{self.first} likes {thing}"
 
 user1 = User("Joe","Smith",68)
 user2 = User("Biana","Lopez",31)
 
 print(user2.full_name())
 print(user1.initials())
+print(user2.likes("ice cream"))
