1ad1d51a865ad1cdf55927b1a11d7f1cde53a87f
Adam Kahn <adamkahn@adamkahn-macbookpro.roam.corp.google.com>
2018-12-13 17:37:16 -0800

    Define init and repr for class Deck

--- cards.py
+++ cards.py
@@ -9,8 +9,28 @@ class Card:
             raise ValueError(f"You can't have {value} as a value")
         self.suit = suit
         self.value = value
-        
+
     def __repr__(self):
         return f"{self.value} of {self.suit}"
 
 class Deck:
+
+    cards_in_deck = 0
+
+    def __init__(self):
+        cards = []
+        allowed_value = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+        allowed_suit = ["Hearts","Diamonds","Clubs","Spades"]
+        i = 0
+        while i < 4:
+            for v in allowed_value:
+                cards.append(str(v + " of " + allowed_suit[i]))
+            i += 1
+        Deck.cards_in_deck = len(cards)
+
+    def __repr__(self):
+        return f"Deck of {self.cards_in_deck} cards"
+
+
+my_deck = Deck()
+print(my_deck)
