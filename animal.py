e9f7ed6e1fb806186719ea9d9b001cc1266a6e7c
Adam Kahn <adamkahn@adamkahn-macbookpro.roam.corp.google.com>
2019-01-08 15:11:54 -0800

    Create Cat class without leveraging inheritance

--- animal.py
+++ animal.py
@@ -1,11 +1,20 @@
 class Animal:
-    cool = True
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+
+    def __repr__(self):
+        return "{} is a {}".format(self.name, self.species)
 
     def make_sound(self, sound):
         print("This animal says {}".format(sound))
 
 class Cat(Animal):
-    pass
+    def __init__(self, name, species, breed, toy):
+        self.name = name
+        self.species = species
+        self.breed = breed
+        self.toy = toy
 
 blue = Cat()
 print(blue.make_sound("meow"))
