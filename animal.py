c231b033b3283e481686a42a057ba6929774c404
Adam Kahn <adamkahn@adamkahn-macbookpro.roam.corp.google.com>
2019-01-08 15:18:34 -0800

    Use super() in Cat class __init__

--- animal.py
+++ animal.py
@@ -9,14 +9,20 @@ class Animal:
     def make_sound(self, sound):
         print("This animal says {}".format(sound))
 
+#you can use super() to init Cat as child of Animal, which runs Animal.__init__
+#and passes in self as the first argument
 class Cat(Animal):
-    def __init__(self, name, species, breed, toy):
-        Animal.__init__(self, name, species)
+    def __init__(self, name, breed, toy):
+        super().__init__(name, species="cat")
         self.breed = breed
         self.toy = toy
 
-blue = Cat("Blue", "cat", "Scottish Fold", "string")
+    def play(self):
+        print(f"{self.name} likes to play with {self.toy}")
+
+blue = Cat("Blue", "Scottish Fold", "string")
 print(blue)
 print(blue.species)
 print(blue.breed)
 print(blue.toy)
+print(blue.play())
